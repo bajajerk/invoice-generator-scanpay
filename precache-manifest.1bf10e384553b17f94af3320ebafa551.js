80dda7914c642d0652e8f8b76a2ee84ec555c47e
Mayank Bajaj <bajaj3743@gmail.com>
2023-04-28 19:32:40 +0530

    Updates

---
+++ precache-manifest.1bf10e384553b17f94af3320ebafa551.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efb7088cbbe67656dda244fdfd0fc693",
+    "url": "/react-invoice-generator/index.html"
+  },
+  {
+    "revision": "20d999d5681d676143d0",
+    "url": "/react-invoice-generator/static/css/2.0d77bc6e.chunk.css"
+  },
+  {
+    "revision": "fd82d2054b63e6c9f04a",
+    "url": "/react-invoice-generator/static/css/main.b4d9d66b.chunk.css"
+  },
+  {
+    "revision": "20d999d5681d676143d0",
+    "url": "/react-invoice-generator/static/js/2.56b184cd.chunk.js"
+  },
+  {
+    "revision": "5fae47d0a016a5b68e2af829b08bec2a",
+    "url": "/react-invoice-generator/static/js/2.56b184cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd82d2054b63e6c9f04a",
+    "url": "/react-invoice-generator/static/js/main.ba1dcc70.chunk.js"
+  },
+  {
+    "revision": "aabb303fe90f96d72bbb",
+    "url": "/react-invoice-generator/static/js/runtime-main.33c79cdc.js"
+  },
+  {
+    "revision": "5af81432dda1dde2def604f8052f424a",
+    "url": "/react-invoice-generator/static/media/download.5af81432.svg"
+  },
+  {
+    "revision": "15c01d9b81ceb6702aa92e3b5416ce50",
+    "url": "/react-invoice-generator/static/media/remove.15c01d9b.svg"
+  },
+  {
+    "revision": "d4da359f65106dae13fb51495f035419",
+    "url": "/react-invoice-generator/static/media/resize.d4da359f.svg"
+  }
+]);
